f65135edd09147ebe68e823ce2a4b3decaea1216
Arslansaifi012 <devarslan457@gmail.com>
2026-03-29 18:39:28 +0530

    modify code

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
             </div>
 
             <div className="my-10 flex flex-col md:flex-row gap-16">
-                <img className="w-full md:max-w-[450px]" src={assets.about_img} alt="" />
+                <img className="w-full md:max-w-[450px]" src={assets.logo1} alt="" />
                 <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
                    
                    <p>Welcome to SaifWud – your trusted destination for premium PVC wicker storage baskets in India.
